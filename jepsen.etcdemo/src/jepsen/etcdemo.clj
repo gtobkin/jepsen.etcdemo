2a8e331eb91bb91cd911a9196983317f3ab2790a
Greg Tobkin <greg.tobkin@gmail.com>
2018-11-28 22:28:06 +0000

    Initial commit: install etcd v3.1.5 on workers

---
+++ jepsen.etcdemo/src/jepsen/etcdemo.clj
@@ -0,0 +1,45 @@
+(ns jepsen.etcdemo
+  (:require [clojure.tools.logging :refer :all]
+            [clojure.string :as str]
+            [jepsen [cli :as cli]
+                    [control :as c]
+                    [db :as db]
+                    [tests :as tests]]
+            [jepsen.control.util :as cu]
+            [jepsen.os.debian :as debian]))
+
+(def dir "/opt/etcd")
+
+(defn db
+  "Constructs a database for the given etcd version."
+  [version]
+  (reify db/DB
+    (setup! [_ test node]
+      (c/su
+        (info "Setting up etcd")
+        (let [url (str "https://storage.googleapis.com/etcd/" version
+                       "/etcd-" version "-linux-amd64.tar.gz")]
+          (cu/install-archive! url dir))))
+
+    (teardown! [_ test node]
+      (info "Tearing down db"))))
+
+(defn etcd-test
+  "Given an options map from the command line runner (e.g. :nodes, :ssh,
+  :concurrency, ...), constructs a test map."
+  [opts]
+  (merge tests/noop-test
+         opts
+         {:name "etd"
+          :db (db "v3.1.5")
+          :os debian/os}))
+
+(defn -main
+  "Handles command line arguments. Can either run a test, or a web server for
+  browsing results."
+  ; This is a comment. Clojure comments are preceded by a semicolon.
+  [& args]
+  (cli/run! (merge (cli/single-test-cmd {:test-fn etcd-test})
+                   (cli/serve-cmd))
+            args))
+
